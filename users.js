9d1d06a9cdffe7b39fe12af81715bab096ff39be
Siavash Ebrahimi <ebrahimi.siavash@gmail.com>
2022-10-05 02:30:21 +0400

    Added users repository

---
+++ users.js
@@ -0,0 +1,15 @@
+let users = [
+  {
+    id: 1,
+    name: 'Siavash',
+    email: 'eb.sh@gil.com',
+    favoritMovies: []
+  },
+  {
+    id: 2,
+    name: 'Kim',
+    email: 'kim.siavash@gmail.com',
+    favoritMovies: ['The Avatar']
+  }
+];
+module.exports = users;
